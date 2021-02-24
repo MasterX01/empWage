aaa66beb1d0cf83149d254031e144cf314749314
MasterX01 <aakashsaxena14@gmail.com>
2021-02-24 15:03:20 +0530

    ADD. added total salary for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,28 @@
 #!/bin/bash -x
 
-ratePerHour=20
+ratePerHr=20
 fullTime=1
 partTime=0
-empCheck=$(( RANDOM%3 ))
-case $empCheck in
-	$fullTime)
-		empHr=8
-	;;
-	$partTime)
-		empHr=4
-	;;
-	*)
-		empHr=0
-	;;
-esac
-echo "Hours Worked - "$empHr
-echo "Wage for the day is: "$(( ratePerHour*empHr ))
+totalSalary=0
+
+noOfWorkingDays=20
+workingDays=1
+
+while (( $workingDays<=$$noOfWorkingDays ))
+do
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$fullTime)
+			empHr=8
+		;;
+		$partTime)
+			empHr=4
+		;;
+		*)
+			empHr=0
+		;;
+	esac
+	(( workingDays++ ))
+	totalSalary=$(( $totalSalary+$(( $empHr*$ratePerHr )) ))
+done
+echo "Total Salary is: $totalSalary"
