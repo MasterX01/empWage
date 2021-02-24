29a19356344a65727f9d2193d57e210bc87c7284
MasterX01 <aakashsaxena14@gmail.com>
2021-02-24 15:12:13 +0530

    ADD. added feature to calculate for the condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,31 @@
 #!/bin/bash -x
 
-ratePerHr=20
-fullTime=1
-partTime=0
-totalSalary=0
-
-noOfWorkingDays=20
-workingDays=1
-
-while (( $workingDays<=$$noOfWorkingDays ))
+partTime=0;
+fullTime=1;
+totalSalary=0;
+noOfWorkingDays=20;
+maxWorkingHours=100;
+empRatepHr=20;
+empHrCheck=0;
+workingDays=1;
+while (( $empHrCheck<=$maxWorkingHours && $workingDays<=$noOfWorkingDays ))
 do
-	empCheck=$(( RANDOM%3 ))
-	case $empCheck in
-		$fullTime)
-			empHr=8
-		;;
-		$partTime)
-			empHr=4
-		;;
-		*)
-			empHr=0
-		;;
-	esac
-	(( workingDays++ ))
-	totalSalary=$(( $totalSalary+$(( $empHr*$ratePerHr )) ))
+        empCheck=$(( RANDOM%3 ));
+                case $empCheck in
+                        $partTime)
+                                empHr=4
+                                ;;
+                        $fullTime)
+                                empHr=8
+                                ;;
+                        *)
+                                empHr=0
+                                ;;
+                esac
+                ((empHrCheck+=empHr))
+                ((workingDays++))
+                salary=$(($empHr*$empRatepHr));
+                totalSalary=$(($totalSalary+$salary));
 done
-echo "Total Salary is: $totalSalary"
+echo "Total Salary is $totalSalary"
+
