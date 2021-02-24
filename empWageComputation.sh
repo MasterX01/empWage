c108829390dd2ad8862fb53823bf77deb1c181c5
MasterX01 <aakashsaxena14@gmail.com>
2021-02-24 15:20:06 +0530

    ADD. added function to calculate empCheck

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,41 @@
 #!/bin/bash -x
 
+#Constant
 partTime=0;
 fullTime=1;
 totalSalary=0;
 noOfWorkingDays=20;
-maxWorkingHours=100;
-empRatepHr=20;
 empHrCheck=0;
 workingDays=1;
-while (( $empHrCheck<=$maxWorkingHours && $workingDays<=$noOfWorkingDays ))
+
+#Variables
+maxWorkingHours=100;
+empRatepHr=20
+
+function empcheck() {
+        case $1 in
+                $partTime)
+                        empHr=4
+                        ;;
+                $fullTime)
+                        empHr=8
+                        ;;
+                *)
+                        empHr=0
+                        ;;
+        esac
+        echo $empHr
+}
+
+while (( $empHrCheck<=$maxWorkingHours && $workingDays<+$noOfWorkingDays ))
 do
-        empCheck=$(( RANDOM%3 ));
-                case $empCheck in
-                        $partTime)
-                                empHr=4
-                                ;;
-                        $fullTime)
-                                empHr=8
-                                ;;
-                        *)
-                                empHr=0
-                                ;;
-                esac
-                ((empHrCheck+=empHr))
-                ((workingDays++))
-                salary=$(($empHr*$empRatepHr));
-                totalSalary=$(($totalSalary+$salary));
+        empHrReturn="$( empcheck $((RANDOM%3)) )"
+        ((empHrCheck+=empHrReturn))
+        ((workingDays++))
+        salary=$(($empHrReturn*$empRatepHr));
+        totalSalary=$(($totalSalary+$salary))
 done
-echo "Total Salary is $totalSalary"
+#salary=$(($empHrCheck*$empRatepHr));
+echo "Total Salary is $totalSalary";
+
 
