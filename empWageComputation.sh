86ce592cae19da112b84670f7304aaedc6d07a03
MasterX01 <aakashsaxena14@gmail.com>
2021-02-24 13:45:29 +0530

    ADD. Added daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
 
+ratePerHour=20
+fulltime=8
+
 if (( $((RANDOM%2)) -eq 1 ))
 then
 	echo "Employee is Present"
+	echo "Wage for the day is: "$(( ratePerHour*fulltime ))
 else
 	echo "Employee is Absent"
 fi
+
